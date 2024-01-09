e13742c06a1a51f850f8a40c526589898b36d60c
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-01-09 20:43:41 +0700

    add movie details

--- 07-usepopcorn-vite-pnpm/src/App.jsx
+++ 07-usepopcorn-vite-pnpm/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import StarRating from './StarRating';
 
 const tempMovieData = [
   {
@@ -58,54 +59,62 @@ export default function App() {
   const [query, setQuery] = useState('interstellar');
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [selectedId, setSelectedId] = useState(null);
 
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
-  useEffect(function () {
-    async function fetchData() {      
+  function handleSelectMovie(id) {
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
+  }
+  function handleCloseMovieDetails() {
+    setSelectedId(null);
+  }
 
-      setError('');
-      setIsLoading(true);
-      try {
-        console.log('fetch data...');
-        const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
-        );
+  useEffect(
+    function () {
+      async function fetchData() {
+        setError('');
+        setIsLoading(true);
+        try {
+          console.log('fetch data...');
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
+          );
 
-        if (!res.ok){
-          throw new Error('Something went wrong when fetching movies');
+          if (!res.ok) {
+            throw new Error('Something went wrong when fetching movies');
+          }
+
+          const data = await res.json();
+
+          console.log('movie data', data);
+
+          if (data.Response === 'False') {
+            throw new Error('Movie not found');
+          } else {
+            setMovies(data.Search);
+          }
+        } catch (err) {
+          console.error('error', err);
+          setError(err.message);
+        } finally {
+          setIsLoading(false);
         }
- 
-        const data = await res.json();
-
-        console.log('movie data', data);
-        
-        if (data.Response === 'False') {
-          throw new Error('Movie not found');
-        } else {
-          setMovies(data.Search);
-        }
-      } catch (err) {            
-        console.error('error', err);    
-        setError(err.message);        
-      } finally {
-        setIsLoading(false);
       }
-    }
 
-    if (query.length <= 3)
-      return;
+      if (query.length <= 3) return;
 
-    fetchData();
-
-  }, [query]);
+      fetchData();
+    },
+    [query]
+  );
 
   return (
     <>
       <Nav>
-        <Search query={query} onChange={(e) => setQuery(e.target.value)}/>
-        <NumResult movies={movies}/>
+        <Search query={query} onChange={(e) => setQuery(e.target.value)} />
+        <NumResult movies={movies} />
       </Nav>
 
       <main className="main">
@@ -115,24 +124,30 @@ export default function App() {
           ) : isLoading == true ? (
             <Loader />
           ) : (
-            <MoviesFound movies={movies} />
+            <MoviesFound movies={movies} onSelectMovie={handleSelectMovie} />
           )}
         </Box>
 
         <Box>
-          <MoviesWatched watched={watched} />
+          {selectedId != null ? (
+            <MovieDetails
+              selectedId={selectedId}
+              onClose={handleCloseMovieDetails}
+            />
+          ) : (
+            <MoviesWatched watched={watched} />
+          )}
         </Box>
       </main>
     </>
   );
 }
 
-
 Nav.propTypes = {
-  children: PropTypes.any
-}
+  children: PropTypes.any,
+};
 
-function Nav({children}) {
+function Nav({ children }) {
   return (
     <nav className="nav-bar">
       <div className="logo">
@@ -144,14 +159,13 @@ function Nav({children}) {
   );
 }
 
-
 Search.propTypes = {
   query: PropTypes.string,
-  onChange: PropTypes.func
-}
+  onChange: PropTypes.func,
+};
 
-function Search({query, onChange}) {
-  return (    
+function Search({ query, onChange }) {
+  return (
     <input
       className="search"
       type="text"
@@ -159,22 +173,20 @@ function Search({query, onChange}) {
       value={query}
       onChange={onChange}
     />
-    
-  )
+  );
 }
 
 NumResult.propTypes = {
-  movies: PropTypes.array
-}
+  movies: PropTypes.array,
+};
 
-function NumResult({movies}) {
+function NumResult({ movies }) {
   return (
     <p className="num-results">
       Found <strong>{movies.length}</strong> results
     </p>
-  )
+  );
 }
-
 
 Box.propTypes = {
   children: PropTypes.any,
@@ -186,9 +198,10 @@ function Box({ children }) {
 
 MoviesFound.propTypes = {
   movies: PropTypes.array,
+  onSelectMovie: PropTypes.func,
 };
 
-function MoviesFound({ movies }) {
+function MoviesFound({ movies, onSelectMovie }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -200,9 +213,9 @@ function MoviesFound({ movies }) {
         {isOpen1 ? '–' : '+'}
       </button>
       {isOpen1 && (
-        <ul className="list">
+        <ul className="list list-movies">
           {movies?.map((movie) => (
-            <li key={movie.imdbID}>
+            <li key={movie.imdbID} onClick={() => onSelectMovie(movie.imdbID)}>
               <img src={movie.Poster} alt={`${movie.Title} poster`} />
               <h3>{movie.Title}</h3>
               <div>
@@ -216,6 +229,81 @@ function MoviesFound({ movies }) {
         </ul>
       )}
     </>
+  );
+}
+
+MovieDetails.propTypes = {
+  selectedId: PropTypes.string,
+  onClose: PropTypes.func,
+};
+
+function MovieDetails({ selectedId, onClose }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function fetchData() {
+        setIsLoading(true);
+        try {
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${API_KEY}&i=${selectedId}`
+          );
+          if (!res.ok) throw new Error('Cannot fetch movie details');
+
+          const data = await res.json();
+          setMovie(data);
+
+          console.log('movie details', data);
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (selectedId !== null) fetchData();
+    },
+    [selectedId]
+  );
+
+  return (
+    <div className="details">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className="btn-back" onClick={onClose}>
+              &larr;
+            </button>
+            <img src={movie.Poster} alt={`Poster of ${movie.Title} movie`} />
+            <div className="details-overview">
+              <h2>{movie.Title}</h2>
+              <p>
+                {movie.Released} &bull; {movie.Runtime}
+              </p>
+              <p>{movie.Genre}</p>
+              <p>
+                <span>⭐️</span>
+                {movie.imdbRating} IMDB Rating
+              </p>
+            </div>
+          </header>
+
+          <section>
+            <div className="rating">
+              <StarRating maxRating={10} size={24} />
+            </div>
+            <p>
+              <em>{movie.Plot}</em>
+            </p>
+            <p>Actors: {movie.Actors}</p>
+            <p>Directed by: {movie.Director}</p>
+          </section>
+        </>
+      )}
+    </div>
   );
 }
 
@@ -292,7 +380,11 @@ ErrorMsg.propTypes = {
 };
 
 function ErrorMsg({ message }) {
-  return <p className="error"><span>⛔️</span> {message}</p>;
+  return (
+    <p className="error">
+      <span>⛔️</span> {message}
+    </p>
+  );
 }
 
 function Loader() {
