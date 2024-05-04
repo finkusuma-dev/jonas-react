e452683f725c75e7824396c9f00c1c9c99e68ce4
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-05-04 14:52:29 +0700

    chore: comments console.log

--- 10-react-quiz/src/components/Question.jsx
+++ 10-react-quiz/src/components/Question.jsx
@@ -9,7 +9,7 @@ Question.propTypes = {
 }
 
 function Question({question, dispatch, answer}) {
-  console.log('question', question);
+  // console.log('question', question);
   return (
     <div>      
       <h4>{question.question}</h4>
