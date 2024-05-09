a94879eb4a29960ad070bcad6db448b291fa1298
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-05-09 16:19:04 +0700

    feat: change styles and some pages (copied from starter files)

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -3,6 +3,8 @@ import Product from './pages/Product';
 import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
 import NotFound from './pages/NotFound';
+import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ 11-worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from '../components/AppNav'
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      App sfaa
+    </div>
+  )
+}
+
+export default AppLayout
