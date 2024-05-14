b97870770784a3614272b5b33ebdb77fd63afc9e
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-05-14 20:33:14 +0700

    feat: clickin on Map navigates to Form & add back button functionality

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -9,6 +9,7 @@ import Login from './pages/Login';
 import CityList from './components/CityList';
 import City from './components/City';
 import CountryList from './components/CountryList';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -34,7 +35,6 @@ function App() {
     fetchCities();
   }, []);
 
-
   return (
     <BrowserRouter>
       <Routes>
@@ -45,7 +45,10 @@ function App() {
 
         <Route path="app" element={<AppLayout />}>
           {/* Nested Routes */}
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
@@ -55,7 +58,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
 
         <Route path="*" element={<NotFound />} />
