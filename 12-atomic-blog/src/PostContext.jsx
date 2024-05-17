62da4f3164ba211ed6a811bc2e63183922e70950
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-05-17 14:44:38 +0700

    refactor: move states & methods to PostContext

---
+++ 12-atomic-blog/src/PostContext.jsx
@@ -0,0 +1,66 @@
+import PropTypes from 'prop-types';
+import { faker } from '@faker-js/faker';
+import React, { useState, useContext } from 'react';
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+const PostContext = React.createContext();
+
+PostProvider.propTypes = {
+  children: PropTypes.any,
+};
+
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState('');
+
+  /// Define context
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        addPost: handleAddPost,
+        clearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+function usePosts() {
+  const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error('useContext is used outside of provider!');
+  return context;
+}
+
+export { PostProvider, usePosts };
