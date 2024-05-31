2a3e9f573895ef86d762dbb17bc893ad912f9dc6
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-05-31 17:31:17 +0700

    feat: add configuration for the browser extension

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
 import { applyMiddleware } from 'redux';
@@ -9,9 +9,11 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk),
+  composeEnhancers(applyMiddleware(thunk)),
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() /// enable browser extension
 );
 
