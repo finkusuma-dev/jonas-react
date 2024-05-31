656f5021cc354930e270f301a4634bbe22fae902
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-05-31 20:56:40 +0700

    refactor: using redux toolkit instead of classic redux

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,20 +1,13 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
-import { applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  }
 });
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk)),
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() /// enable browser extension
-);
 
 export default store;
