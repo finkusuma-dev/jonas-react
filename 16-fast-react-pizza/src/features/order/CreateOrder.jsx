c7796f6af6f1dc963823c089fd5dc8e2f9bf6766
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-06-11 20:58:54 +0700

    fix: fix h2 text

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -44,7 +44,7 @@ function CreateOrder() {
 
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      <h2>Ready to order? Let&apos;s go!</h2>
 
       <Form method="POST">
         <div>
