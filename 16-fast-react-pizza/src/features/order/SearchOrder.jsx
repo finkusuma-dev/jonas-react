f84e5067510837ebadb8f479ffcfa2274f65f6ad
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-06-12 13:54:02 +0700

    style: using roboto mono as the default sans font
    Also change the width of the search order so it won't clip the placeholder text

--- 16-fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 16-fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -17,7 +17,7 @@ function SearchOrder() {
           placeholder="Enter order #"
           onChange={(e) => setQuery(e.target.value)}
           value={query}
-          className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-56 sm:focus:w-72"
+          className="w-32 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-56 sm:focus:w-72"
         />
       </form>
     </div>
--- 16-fast-react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    /// Writing our fontFamily config here will overwrite default config,
+    /// so we will no longer able to use for example: font-serif
+    ///
+    extend: {
+      /// Adding our own config to the default config
+      ///
+      fontFamily: {
+        sans: 'Roboto Mono, monospace',
+      },
+      fontSize: {
+        big: ['10rem', { lineHeight: '1' }],
+      },
+    },
   },
   plugins: [],
-}
-
+};
