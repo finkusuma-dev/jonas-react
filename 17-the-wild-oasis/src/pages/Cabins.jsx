63e474260a386432f4160246e4dbedf6e3d6bb68
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-07-15 20:24:06 +0700

    feat: load cabins' data from supabase.
    - Add package @supabase/supabase-js.
    - Add supabase.js to create Supabase client.
    - Add apiCabins.js to use the client and get the cabins' data.

--- 17-the-wild-oasis/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/src/pages/Cabins.jsx
@@ -1,7 +1,13 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { useEffect } from 'react';
+import { getCabins } from '../services/apiCabins';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
 
 function Cabins() {
+  useEffect(function () {
+    getCabins().then((data) => console.log('cabins', data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
--- 17-the-wild-oasis/src/pages/Dashboard.jsx
+++ 17-the-wild-oasis/src/pages/Dashboard.jsx
@@ -1,7 +1,12 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+// import { useEffect } from 'react';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
 
 function Dashboard() {
+  // useEffect(function () {
+  //   console.log('env', import.meta.env);
+  // });
+
   return (
     <Row type="horizontal">
       <Heading as="h1">Dashboard</Heading>
