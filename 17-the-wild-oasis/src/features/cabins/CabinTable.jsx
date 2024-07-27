0b60e54945c57709345ee925104b834b054f6b27
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-07-27 13:47:47 +0700

    refactor(cabin): move `useCabins` from Cabin to CabinTable

--- 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,37 +1,17 @@
-import PropTypes from 'prop-types';
+import { useCabins } from '../../features/cabins/useCabins';
+import Spinner from '../../ui/Spinner';
+
 import CabinRow from './CabinRow';
 import Table from '../../ui/Table';
 import Menus from '../../ui/Menus';
 
-// const Table = styled.div`
-//   border: 1px solid var(--color-grey-200);
+function CabinTable() {
+  const { isLoading, cabins } = useCabins();
 
-//   font-size: 1.4rem;
-//   background-color: var(--color-grey-0);
-//   border-radius: 7px;
-//   overflow: hidden;
-// `;
+  console.log('cabins', cabins);
 
-// const TableHeader = styled.header`
-//   display: grid;
-//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-//   column-gap: 2.4rem;
-//   align-items: center;
+  if (isLoading) return <Spinner />;
 
-//   background-color: var(--color-grey-50);
-//   border-bottom: 1px solid var(--color-grey-100);
-//   text-transform: uppercase;
-//   letter-spacing: 0.4px;
-//   font-weight: 600;
-//   color: var(--color-grey-600);
-//   padding: 1.6rem 2.4rem;
-// `;
-
-CabinTable.propTypes = {
-  cabins: PropTypes.array,
-};
-
-function CabinTable({ cabins }) {
   return (
     <>
       <Menus>
--- 17-the-wild-oasis/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/src/pages/Cabins.jsx
@@ -1,20 +1,10 @@
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
 import CabinTable from '../features/cabins/CabinTable';
-import Spinner from '../ui/Spinner';
 
-import { useCabins } from '../features/cabins/useCabins';
 import AddCabin from '../features/cabins/AddCabin';
-// import Modal from '../ui/Modal';
-// import Button from '../ui/Button';
 
 function Cabins() {
-  const { isLoading, cabins } = useCabins();
-
-  console.log('cabins', cabins);
-
-  if (isLoading) return <Spinner />;
-
   return (
     <>
       <Row type="horizontal">
@@ -23,7 +13,7 @@ function Cabins() {
       </Row>
       <Row>
         <AddCabin />
-        <CabinTable cabins={cabins} />
+        <CabinTable />
       </Row>
     </>
   );
