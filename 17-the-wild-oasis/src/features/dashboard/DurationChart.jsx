74f3c6e1d122d6ea55e67c048060351963403a3b
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-08-19 15:12:06 +0700

    refactor(DurationChart): using different approach in startData & prepareData function.
    - Change `duration` to be just a number. Add `durationLabel` later by using current el and previous array el.
    - Put together both light and dark mode in the same startData object.

--- 17-the-wild-oasis/src/features/dashboard/DurationChart.jsx
+++ 17-the-wild-oasis/src/features/dashboard/DurationChart.jsx
@@ -22,118 +22,208 @@ const ChartBox = styled.div`
   }
 `;
 
-const startDataLight = [
+// const startDataLight = [
+//   {
+//     duration: '1 night',
+//     value: 0,
+//     color: '#ef4444',
+//   },
+//   {
+//     duration: '2 nights',
+//     value: 0,
+//     color: '#f97316',
+//   },
+//   {
+//     duration: '3 nights',
+//     value: 0,
+//     color: '#eab308',
+//   },
+//   {
+//     duration: '4-5 nights',
+//     value: 0,
+//     color: '#84cc16',
+//   },
+//   {
+//     duration: '6-7 nights',
+//     value: 0,
+//     color: '#22c55e',
+//   },
+//   {
+//     duration: '8-14 nights',
+//     value: 0,
+//     color: '#14b8a6',
+//   },
+//   {
+//     duration: '15-21 nights',
+//     value: 0,
+//     color: '#3b82f6',
+//   },
+//   {
+//     duration: '21+ nights',
+//     value: 0,
+//     color: '#a855f7',
+//   },
+// ];
+
+// const startDataDark = [
+//   {
+//     duration: '1 night',
+//     value: 0,
+//     color: '#b91c1c',
+//   },
+//   {
+//     duration: '2 nights',
+//     value: 0,
+//     color: '#c2410c',
+//   },
+//   {
+//     duration: '3 nights',
+//     value: 0,
+//     color: '#a16207',
+//   },
+//   {
+//     duration: '4-5 nights',
+//     value: 0,
+//     color: '#4d7c0f',
+//   },
+//   {
+//     duration: '6-7 nights',
+//     value: 0,
+//     color: '#15803d',
+//   },
+//   {
+//     duration: '8-14 nights',
+//     value: 0,
+//     color: '#0f766e',
+//   },
+//   {
+//     duration: '15-21 nights',
+//     value: 0,
+//     color: '#1d4ed8',
+//   },
+//   {
+//     duration: '21+ nights',
+//     value: 0,
+//     color: '#7e22ce',
+//   },
+// ];
+
+const startData = [
   {
-    duration: '1 night',
+    duration: 1,
     value: 0,
-    color: '#ef4444',
+    colorLight: '#ef4444',
+    colorDark: '#b91c1c',
   },
   {
-    duration: '2 nights',
+    duration: 2,
     value: 0,
-    color: '#f97316',
+    colorLight: '#f97316',
+    colorDark: '#c2410c',
   },
   {
-    duration: '3 nights',
+    duration: 3,
     value: 0,
-    color: '#eab308',
+    colorLight: '#eab308',
+    colorDark: '#a16207',
   },
   {
-    duration: '4-5 nights',
+    duration: 5,
     value: 0,
-    color: '#84cc16',
+    colorLight: '#84cc16',
+    colorDark: '#4d7c0f',
   },
   {
-    duration: '6-7 nights',
+    duration: 7,
     value: 0,
-    color: '#22c55e',
+    colorLight: '#22c55e',
+    colorDark: '#15803d',
   },
   {
-    duration: '8-14 nights',
+    duration: 14,
     value: 0,
-    color: '#14b8a6',
+    colorLight: '#14b8a6',
+    colorDark: '#0f766e',
   },
   {
-    duration: '15-21 nights',
+    duration: 21,
     value: 0,
-    color: '#3b82f6',
+    colorLight: '#3b82f6',
+    colorDark: '#1d4ed8',
   },
   {
-    duration: '21+ nights',
+    duration: Infinity,
     value: 0,
-    color: '#a855f7',
+    colorLight: '#a855f7',
+    colorDark: '#7e22ce',
   },
 ];
 
-const startDataDark = [
-  {
-    duration: '1 night',
-    value: 0,
-    color: '#b91c1c',
-  },
-  {
-    duration: '2 nights',
-    value: 0,
-    color: '#c2410c',
-  },
-  {
-    duration: '3 nights',
-    value: 0,
-    color: '#a16207',
-  },
-  {
-    duration: '4-5 nights',
-    value: 0,
-    color: '#4d7c0f',
-  },
-  {
-    duration: '6-7 nights',
-    value: 0,
-    color: '#15803d',
-  },
-  {
-    duration: '8-14 nights',
-    value: 0,
-    color: '#0f766e',
-  },
-  {
-    duration: '15-21 nights',
-    value: 0,
-    color: '#1d4ed8',
-  },
-  {
-    duration: '21+ nights',
-    value: 0,
-    color: '#7e22ce',
-  },
-];
+function prepareData(startData, stays, isDarkMode) {
+  /// Assign dataStart to data.
+  /// Using {... obj } so it copies the object's content and not the reference to the obj.
+  /// This is needed to initiate the data because we will mutate the data.
+  let data = [...startData.map((obj) => ({ ...obj }))];
 
-function prepareData(startData, stays) {
-  // A bit ugly code, but sometimes this is what it takes when working with real data 😅
+  for (let i = 0; i < data.length; i++) {
+    /// Create duration Label. Using IIFE here :).
+    data[i].durationLabel = (() => {
+      if (i === 0) return `${data[i].duration} night`; // 1 night
+      else if (i === data.length - 1)
+        return `${data[i - 1].duration}+ nights`; // 21+ nights
+      else if (data[i - 1].duration + 1 === data[i].duration)
+        return `${data[i].duration} nights`; // 2 nights, 3 nights
+      else return `${data[i - 1].duration + 1}-${data[i].duration} nights`; // 4-5 nights
+    })();
 
-  function incArrayValue(arr, field) {
-    return arr.map((obj) =>
-      obj.duration === field ? { ...obj, value: obj.value + 1 } : obj
-    );
+    /// Set color based on dark mode
+    data[i].color = isDarkMode ? data[i].colorDark : data[i].colorLight;
   }
 
-  const data = stays
-    ?.reduce((arr, cur) => {
-      const num = cur.num_nights;
-      if (num === 1) return incArrayValue(arr, '1 night');
-      if (num === 2) return incArrayValue(arr, '2 nights');
-      if (num === 3) return incArrayValue(arr, '3 nights');
-      if ([4, 5].includes(num)) return incArrayValue(arr, '4-5 nights');
-      if ([6, 7].includes(num)) return incArrayValue(arr, '6-7 nights');
-      if (num >= 8 && num <= 14) return incArrayValue(arr, '8-14 nights');
-      if (num >= 15 && num <= 21) return incArrayValue(arr, '15-21 nights');
-      if (num >= 21) return incArrayValue(arr, '21+ nights');
-      return arr;
-    }, startData)
-    .filter((obj) => obj.value > 0);
+  /// Calculate value
+  stays?.forEach((stay) => {
+    const num = stay?.num_nights;
 
-  return data;
+    /// Loop through the data to compare with the duration
+    /// and then break once condition is met.
+    for (let i = 0; i < data?.length; i++) {
+      if (num <= data?.[i].duration) {
+        data[i].value++;
+        break;
+      }
+    }
+  });
+
+  /// Remove any data element where value = 0
+  return data.filter((el) => el.value > 0);
 }
+
+// function prepareData0(startData, stays) {
+//   // A bit ugly code, but sometimes this is what it takes when working with real data 😅
+
+//   function incArrayValue(arr, field) {
+//     return arr.map((obj) =>
+//       obj.duration === field ? { ...obj, value: obj.value + 1 } : obj
+//     );
+//   }
+
+//   const data = stays
+//     ?.reduce((arr, cur) => {
+//       const num = cur.num_nights;
+//       if (num === 1) return incArrayValue(arr, '1 night');
+//       if (num === 2) return incArrayValue(arr, '2 nights');
+//       if (num === 3) return incArrayValue(arr, '3 nights');
+//       if ([4, 5].includes(num)) return incArrayValue(arr, '4-5 nights');
+//       if ([6, 7].includes(num)) return incArrayValue(arr, '6-7 nights');
+//       if (num >= 8 && num <= 14) return incArrayValue(arr, '8-14 nights');
+//       if (num >= 15 && num <= 21) return incArrayValue(arr, '15-21 nights');
+//       if (num >= 21) return incArrayValue(arr, '21+ nights');
+//       return arr;
+//     }, startData)
+//     .filter((obj) => obj.value > 0);
+
+//   return data;
+// }
 
 DurationChart.propTypes = {
   confirmedStays: PropTypes.array,
@@ -142,20 +232,22 @@ DurationChart.propTypes = {
 function DurationChart({ confirmedStays }) {
   const { isDarkMode } = useDarkMode();
   // console.log('Duration Chart', confirmedStays);
-  const data = prepareData(
-    isDarkMode ? startDataDark : startDataLight,
-    confirmedStays
-  );
 
-  // const colors = isDarkMode
-  //   ? {
-  //       text: '#e5e7eb',
-  //       background: '#18212f',
-  //     }
-  //   : {
-  //       text: '#374151',
-  //       background: '#fff',
-  //     };
+  // console.log(
+  //   // prepareData0(
+  //   //   isDarkMode ? startDataDark : startDataLight,
+  //   //   confirmedStays
+  //   // ).map((el) => ({
+  //   //   duration: el.duration,
+  //   //   value: el.value,
+  //   // })),
+  //   prepareData(dataStart, confirmedStays, isDarkMode).map((el) => ({
+  //     durationLabel: el.durationLabel,
+  //     value: el.value,
+  //   }))
+  // );
+
+  const data = prepareData(startData, confirmedStays, isDarkMode);
 
   return (
     <ChartBox>
@@ -171,7 +263,9 @@ function DurationChart({ confirmedStays }) {
             paddingAngle={3}
             cx="50%"
             cy="45%"
-            label={({ index, value }) => `${data[index].duration}: ${value}`}
+            label={({ index, value }) =>
+              `${data[index].durationLabel}: ${value}`
+            }
             // labelLine={false}
           >
             {data?.map((item) => (
