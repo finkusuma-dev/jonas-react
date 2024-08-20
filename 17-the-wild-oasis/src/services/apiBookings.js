c7dd4c02bba22c827d30bf0bf53c5a4286800436
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-08-20 15:18:06 +0700

    fix(apiBookings.js): rename table name on select function on getStaysTodayActivity

--- 17-the-wild-oasis/src/services/apiBookings.js
+++ 17-the-wild-oasis/src/services/apiBookings.js
@@ -99,9 +99,9 @@ export async function getStaysAfterDate(date) {
 export async function getStaysTodayActivity() {
   const { data, error } = await supabase
     .from('bookings')
-    .select('*, guests(fullName, nationality, countryFlag)')
+    .select('*, guests(full_name, nationality, country_flag)')
     .or(
-      `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
+      `and(status.eq.unconfirmed,start_date.eq.${getToday()}),and(status.eq.checked-in,end_date.eq.${getToday()})`
     )
     .order('created_at');
 
@@ -117,6 +117,7 @@ export async function getStaysTodayActivity() {
 }
 
 export async function updateBooking(id, obj) {
+  // console.log('updateBooking api', id, obj);
   const { data, error } = await supabase
     .from('bookings')
     .update(obj)
