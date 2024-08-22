6ddb721e1f31077b833d18a55ee48c52ce3f3af6
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-08-22 14:37:17 +0700

    fix(AddCabin.jsx): remove Row and Heading components that make cabin form modal not close after success

--- 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,6 +1,4 @@
 import PropTypes from 'prop-types';
-import Heading from '../../ui/Heading';
-import Row from '../../ui/Row';
 import CreateCabinForm from './CreateCabinForm';
 import Modal from '../../ui/Modal';
 import Button from '../../ui/Button';
@@ -8,25 +6,16 @@ import Button from '../../ui/Button';
 
 AddCabin.propTypes = {
   cabinToEdit: PropTypes.object,
-  onCloseModal: PropTypes.func,
 };
 
-function AddCabin({ cabinToEdit = null, onCloseModal }) {
+function AddCabin({ cabinToEdit = null }) {
   return (
     <Modal>
       <Modal.Open name="add-cabin">
         <Button>Add new cabin</Button>
       </Modal.Open>
       <Modal.Window name="add-cabin">
-        <Row>
-          <Heading as="h2">
-            {cabinToEdit ? 'Edit cabin' : 'Add A New Cabin'}
-          </Heading>
-          <CreateCabinForm
-            onCloseModal={onCloseModal}
-            cabinToEdit={cabinToEdit}
-          />
-        </Row>
+        <CreateCabinForm cabinToEdit={cabinToEdit} />
       </Modal.Window>
     </Modal>
   );
